61f52fa8bc019fc12b5f244c9044e50e43ec257a
Kendarg Real <estebanreal2010@hotmail.com>
2026-06-29 00:42:42 -0500

    Cargar Proyectos Generation

---
+++ taller1.js
@@ -0,0 +1,13 @@
+let info = {
+nombre: "Esteban",
+ciudad: "Bogota"
+}
+let {nombre, ciudad} = info;
+let rappiPro = true;
+if(!rappiPro){
+console.log(`Hola tu nombre es ${nombre}  \nVives en ${ciudad}  \nEres rappi pro`);
+
+}else{
+console.log(`Hola tu nombre es ${nombre}  \nVives en ${ciudad}`);
+
+}
