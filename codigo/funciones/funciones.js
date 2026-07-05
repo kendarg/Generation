c9bf188d6e429b310a0f930fdc19dd38014d0213
Kendarg Real <estebanreal2010@hotmail.com>
2026-07-05 01:06:10 -0500

    Eliminar carpetas

---
+++ codigo/funciones/funciones.js
@@ -0,0 +1,8 @@
+function calcularDescuento(precio) {
+  const precioFinal = precio - (precio * 0.20);
+    console.log("Precio final: " + precioFinal);
+}
+
+calcularDescuento(45000);
+calcularDescuento(120000);
+calcularDescuento(89900);
